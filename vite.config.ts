941e1ce44bdbba72ce962cc2bb4fc74803e68141
john-mirage <chaosdynamix@posteo.net>
2022-03-24 18:24:47 +0100

    feat: add base path for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/order-summary-component/",
     plugins: [react()],
     resolve: {
         alias: {
